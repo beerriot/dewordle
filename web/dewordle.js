84920ab02483047eccaee5ea5819864d0ca9d6c8
Bryan Fink <bryan.fink@gmail.com>
2022-01-26 16:03:34 -0600

    demo add squares to game

--- web/dewordle.js
+++ web/dewordle.js
@@ -13,8 +13,35 @@ yellow.setAttribute("class", "almost");
 var green = square.cloneNode(true);
 green.setAttribute("class", "correct");
 
+// example board
 board.append(white.cloneNode(true));
 board.append(yellow.cloneNode(true));
 board.append(green.cloneNode(true));
 board.append(yellow.cloneNode(true));
 board.append(white.cloneNode(true));
+
+// inputs
+var inputArea = document.getElementById("gameinput");
+
+var whiteInput = white.cloneNode(true);
+var yellowInput = yellow.cloneNode(true);
+var greenInput = green.cloneNode(true);
+
+inputArea.append(whiteInput);
+inputArea.append(yellowInput);
+inputArea.append(greenInput);
+
+function inputDownHandler(ev) {
+    this.setPointerCapture(ev.pointerId);
+}
+
+function inputUpHandler(ev) {
+    board.append(this.cloneNode(true));
+}
+
+whiteInput.onpointerdown = inputDownHandler;
+whiteInput.onpointerup = inputUpHandler;
+yellowInput.onpointerdown = inputDownHandler;
+yellowInput.onpointerup = inputUpHandler;
+greenInput.onpointerdown = inputDownHandler;
+greenInput.onpointerup = inputUpHandler;
