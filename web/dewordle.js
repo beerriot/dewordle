02487168a89c9a6aae66866e80b6350bd1735d91
Bryan Fink <bryan.fink@gmail.com>
2022-01-26 11:57:30 -0600

    skeleton of web dewordle

---
+++ web/dewordle.js
@@ -0,0 +1,5 @@
+var square = document.getElementById("gamesquare");
+square.after(square.cloneNode(true));
+square.after(square.cloneNode(true));
+square.after(square.cloneNode(true));
+square.after(square.cloneNode(true));
